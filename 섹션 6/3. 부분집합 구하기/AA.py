6eac4417442a8e947b676f41e8c88e3f5a02d2fd
han <hanocator@gmail.com>
2021-04-20 23:04:02 +0900

    DFS

--- 섹션 6/3. 부분집합 구하기/AA.py
+++ 섹션 6/3. 부분집합 구하기/AA.py
@@ -4,19 +4,19 @@ sys.stdin = open("in1.txt", "r")
 
 
 def DFS(v):
-    if v == n + 1:
+    if v == n + 1:  # n까지는 출력이 되야 함으로 n+1로 계산.
         for i in range(1, n + 1):
-            if ch[i] == 1:
+            if ch[i] == 1:  # 사용하는 경우 출력.
                 print(i, end=' ')
         print()
     else:
-        ch[v] = 1
+        ch[v] = 1  # 사용한다.
         DFS(v + 1)
-        ch[v] = 0
+        ch[v] = 0  # 사용하지 않는다.
         DFS(v + 1)
 
 
 if __name__ == "__main__":
     n = int(input())
-    ch = [0] * (n + 1)
+    ch = [0] * (n + 1)  # 사용 할지 않할지 체크하는 변수.
     DFS(1)
